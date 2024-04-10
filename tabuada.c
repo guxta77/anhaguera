753e8db33d578c9ebf2ec33a926f3a6c3590f6b9
guxta77 <113487751+guxta77@users.noreply.github.com>
2024-04-10 18:24:08 -0300

    Add files via upload

---
+++ tabuada.c
@@ -0,0 +1,18 @@
+//Siga @gustavotrindade77
+#include <stdio.h>
+int main(void)
+
+{
+int numero, cont=0;
+    
+    printf("=====TABUADA=====\n");
+    printf("Digite um numero: ");
+    scanf("%d",&numero);
+
+    for (cont=0; cont<=10 ; cont++)
+        {
+        printf("%d x %d = %d \n",numero,cont,numero*cont);
+        }
+return 0;
+}
+
